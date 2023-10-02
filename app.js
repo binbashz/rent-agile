6422e5ee66bc37feab8f70360c0e464472236e08
binbashz <5732@holbertonstudents.com>
2023-10-02 04:05:50 -0300

    updates

--- app.js
+++ app.js
@@ -53,17 +53,18 @@ app.get('/registro', (req, res) => {
     res.render('registro');
 });
 
-// Ruta para el perfil
+// Ruta para el perfil del usuario
 app.get('/perfil', (req, res) => {
     if (req.session.loggedin) {
         res.render('perfil', {
-            user: req.session.name
+            user: req.session.name, // muestra datos del usuario en su perfil
+            name: req.session.name, // muestra datos del usuario en su perfil
+            email: req.session.email // muestra datos del usuario en su perfil
         });
     } else {
         res.redirect('/login');
     }
 });
-
 //Ruta para el registro ****************
 
 app.post('/registro', async (req, res) => {
@@ -135,6 +136,8 @@ app.post('/registro', async (req, res) => {
         }
     });
 });
+
+
 
 // auth ****
 
