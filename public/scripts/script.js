279fc7fb4f805fa456d202c142a51c55e531d6dc
binbashz <5732@holbertonstudents.com>
2023-10-02 19:02:14 -0300

    updates III

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,9 +1,26 @@
-// script para el slider del index, galeria de autos
+// Obtén una referencia al elemento de superposición de texto y la caja de fondo
+const textContainer = document.querySelector('.text-container');
+const overlayText = document.querySelector('#overlay-text');
 
+// Función para mostrar la superposición de texto y la caja de fondo
+function showTextOverlay() {
+    textContainer.style.display = 'block';
+    overlayText.style.opacity = '1';
+}
+
+// Ocultar la caja de fondo y el texto al principio
+textContainer.style.display = 'none';
+overlayText.style.opacity = '0';
+
+// Mostrar la superposición después de un retraso (por ejemplo, 2 segundos)
+setTimeout(showTextOverlay, 2000);
+
+// script para el slider del index, galería de autos
 let slideIndex = 0;
 
 function showSlide(index) {
-    const slides = document.querySelectorAll('.slide');
+    const slides = document.querySelectorAll('.slider-item');
+    
     if (index >= slides.length) {
         slideIndex = 0;
     } else if (index < 0) {
@@ -11,7 +28,11 @@ function showSlide(index) {
     }
 
     slides.forEach((slide, i) => {
-        slide.style.transform = `translateX(${100 * (i - slideIndex)}%)`;
+        if (i === slideIndex) {
+            slide.style.display = 'block';
+        } else {
+            slide.style.display = 'none';
+        }
     });
 }
 
@@ -25,9 +46,8 @@ function prevSlide() {
     showSlide(slideIndex);
 }
 
-document.getElementById('next').addEventListener('click', nextSlide);
-document.getElementById('prev').addEventListener('click', prevSlide);
-
 // Iniciar slider automáticamente
 setInterval(nextSlide, 2000); // Cambiar cada 2 segundos
 
+// Mostrar el primer slide al cargar la página
+showSlide(slideIndex);
